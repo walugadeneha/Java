a6fbec5b9bd66b69c544eb5335a614e1176b739f
Neha Walugade <108685569+walugadeneha@users.noreply.github.com>
2023-05-06 21:12:08 +0530

    Update SumNos.java

--- Java new coeds/SumNos.java
+++ Java new coeds/SumNos.java
@@ -2,5 +2,22 @@
 
 import java.util.*;
 class SumNos
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i,n,sum=0;
+i=1;
+System.out.println("Enter n:");
+n=sc.nextInt();
+
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println("Sum="+sum);
+}
+}
+
 
 }
